8cca018dae2409a8973956805a3a07e77ccd5e70
Dan Kiss <flowfast47@gmail.com>
2018-03-08 15:26:01 -0800

    setup js practice with node

---
+++ hello.js
@@ -0,0 +1,8 @@
+var epicodusStudent = {
+  firstName: 'Charlie',
+  lastName: 'Bucket',
+  level: 1,
+  track: ['php', 'javascript', 'drupal'],
+  enrollmentStatus: true
+};
+console.log(epicodusStudent);
